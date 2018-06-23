5a7d289aee632aaa0273835c1abf673b40a7b1b7
Sara Broad <saraebroad@gmail.com>
2018-06-23 16:41:34 -0400

    index.js defaults added

--- client/src/pages/Mainpage/Mainpage.js
+++ client/src/pages/Mainpage/Mainpage.js
@@ -1,4 +1,8 @@
 import React, { component } from 'react';
+import Header from "../components/Header";
+import ResultsBar from "../components/ResultsBar";
+import SavedArticles from "../components/SavedArticles";
+import SearchBar from "../components/SearchBar";
 
 class Mainpage extends Component {
     render() {
