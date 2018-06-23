05abb215b6c00549548d4d8c26d54a39cf3af494
Sara Broad <saraebroad@gmail.com>
2018-06-23 16:33:48 -0400

    main page change

---
+++ client/src/pages/Mainpage/Mainpage.js
@@ -0,0 +1,11 @@
+import React, { component } from 'react';
+
+class Mainpage extends Component {
+    render() {
+        return (
+            <p>Hello</p>
+        )
+    }
+}
+
+export default Mainpage;
