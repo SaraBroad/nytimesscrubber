89cdcacfe68adce07d5c03315664ed7bd6d584f3
Sara Broad <saraebroad@gmail.com>
2018-06-23 20:41:58 -0400

    util routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,4 @@
-// var router = require("express").Router();
-// var db = require("../models/Article");
+
 
 
 // /api/articles (get) - your components will use this to query MongoDB for all saved articles
@@ -9,11 +8,37 @@
 // /api/articles (delete) - your components will use this to delete a saved article in the database
 
 // * (get) - will load your single HTML page (with ReactJS) in client/build/index.html. Make sure you put this after all other GET routes
-
-
-
 import axios from "axios";
 
-export default {
-    
+const scrubber = {
+    runQuery: function (searchTerm, startYear, endYear) {
+        const apiKey = "5d7640653b3b4acab5beec69a66e0904"
+        const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+            apiKey + "&q=" + topic + "&begin_date=" + startYear + "0101";
+        return axios.get(queryURL);
+    },
+    getSaved: function () {
+        return axios.get("/api/savedarticles")
+    },
+
+
 }
+
+
+export default scrubber;
+
+//   getBooks: function() {
+//     return axios.get("/api/books");
+//   },
+//   // Gets the book with the given id
+//   getBook: function(id) {
+//     return axios.get("/api/books/" + id);
+//   },
+//   // Deletes the book with the given id
+//   deleteBook: function(id) {
+//     return axios.delete("/api/books/" + id);
+//   },
+//   // Saves a book to the database
+//   saveBook: function(bookData) {
+//     return axios.post("/api/books", bookData);
+//   }
