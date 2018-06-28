809bad96831d0a816babb22e0613c31f9503e9ba
Sara Broad <saraebroad@gmail.com>
2018-06-28 17:09:27 -0400

    fixed css

--- client/src/components/SavedArticlesBarCard/SavedArticlesBarCard.js
+++ client/src/components/SavedArticlesBarCard/SavedArticlesBarCard.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./SavedArticlesBarCard.css";
 
 const SavedArticlesBarCard = props => (
-    <div className="search-container">
+    <div className="saved-container">
         <div className="saved-box">
         <div>{props.title}</div>
         <div>{props.url}</div>
         <div>{props.date}</div>
-            <button className="btn btn-primary" onClick={() => props.deleteArticles(props._id)}>Delete</button>
+            <button className="btn btn-primary delete-btn" onClick={() => props.deleteArticles(props._id)}>Delete</button>
         </div>
     </div>
 );
--- client/src/components/SavedBarWrapper/SavedBarWrapper.js
+++ client/src/components/SavedBarWrapper/SavedBarWrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./SavedBarWrapper.css";
 
-const SavedBarWrapper = props => <div className="wrapper">
+const SavedBarWrapper = props => <div className="results-wrapper">
 <h1 className="header">Saved Articles</h1>
 
 {props.children}</div>;
