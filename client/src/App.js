061de214bf5b2fb024b1200e1fc0e02d2a5e0acd
Sara Broad <saraebroad@gmail.com>
2018-06-28 17:11:39 -0400

    removed nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavBar from "./components/NavBar";
+// import NavBar from "./components/NavBar";
 import Mainpage from "./pages/Mainpage";
-// import Saved from "./pages/Saved";
-
 const App = () => (
   <Router>
     <div>
-    <NavBar />
+    {/* <NavBar /> */}
       <Switch>
         <Route exact path="/" component={Mainpage} />
-        {/* <Route exact path="/saved" component={Saved} /> */}
       </Switch>
     </div>
   </Router>
